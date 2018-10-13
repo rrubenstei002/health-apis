0cc3066167369f70568d106e54ffe18e6ced2738
Bryan Schofield <bryan.schofield@libertyits.com>
2018-10-13 13:11:50 -0400

    Removed Jackson annotations from ids-api data structures

--- ids-api/src/main/java/gov/va/api/health/ids/api/Registration.java
+++ ids-api/src/main/java/gov/va/api/health/ids/api/Registration.java
@@ -1,12 +1,10 @@
 package gov.va.api.health.ids.api;
 
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import java.util.List;
 import lombok.Builder;
 import lombok.Singular;
 import lombok.Value;
 
-@JsonDeserialize(builder = Registration.RegistrationBuilder.class)
 @Value
 @Builder
 public class Registration {
