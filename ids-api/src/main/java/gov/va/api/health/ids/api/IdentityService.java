dead2ac9bbac64c966e97d7073467ddbbe49a229
Bryan Schofield <bryan.schofield@libertyits.com>
2018-10-21 22:00:24 -0400

    Experimental branch to generate swagger from ID service interfaces

--- ids-api/src/main/java/gov/va/api/health/ids/api/IdentityService.java
+++ ids-api/src/main/java/gov/va/api/health/ids/api/IdentityService.java
@@ -1,11 +1,74 @@
 package gov.va.api.health.ids.api;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import java.util.List;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
 
+@OpenAPIDefinition(
+    info =
+        @Info(
+            title = "Identity Service",
+            version = "v1",
+            description = "Performs mapping from public IDs to internal system IDs."))
+@Path("api/v1/ids")
 public interface IdentityService {
 
-  List<ResourceIdentity> lookup(String id);
+  @Operation(
+      summary = "Look up a resource identity",
+      description =
+          "Look up a resource identity using the public ID "
+              + "determined from a previous registration.")
+  @GET
+  @Path("/{id}")
+  @ApiResponse(
+      responseCode = "200",
+      description = "Resource identity found",
+      content =
+          @Content(
+              mediaType = "application/json",
+              array = @ArraySchema(schema = @Schema(implementation = ResourceIdentity.class))))
+  @ApiResponse(responseCode = "400", description = "Not found")
+  @ApiResponse(responseCode = "404", description = "Bad request")
+  List<ResourceIdentity> lookup(
+      @Parameter(
+              in = ParameterIn.PATH,
+              name = "id",
+              required = true,
+              description = "The public UUD returned in a previous registration.",
+              schema = @Schema(format = "uuid"))
+          String id);
 
+  @Operation(
+      summary = "Register one or more identities",
+      description =
+          "Register resource identities. This operation is idempotent. "
+              + "The returned registration UUID can be used to lookup resource identities.")
+  @POST
+  @RequestBody(
+      description = "Resource identities to register",
+      content =
+          @Content(
+              mediaType = "application/json",
+              array = @ArraySchema(schema = @Schema(implementation = ResourceIdentity.class))))
+  @ApiResponse(
+      responseCode = "201",
+      description = "Registration created",
+      content =
+          @Content(
+              mediaType = "application/json",
+              array = @ArraySchema(schema = @Schema(implementation = Registration.class))))
+  @ApiResponse(responseCode = "404", description = "Bad request")
   List<Registration> register(List<ResourceIdentity> identities);
 
   class IdentityServiceException extends RuntimeException {
--- ids-api/src/main/java/gov/va/api/health/ids/api/ResourceIdentity.java
+++ ids-api/src/main/java/gov/va/api/health/ids/api/ResourceIdentity.java
@@ -1,15 +1,32 @@
 package gov.va.api.health.ids.api;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import io.swagger.v3.oas.annotations.media.Schema;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 import lombok.Builder;
 import lombok.Value;
 
 @Value
 @Builder(toBuilder = true)
 @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
+@Schema(
+  description =
+      "A identifier for a particular resource type within a specific system."
+          + " Identifiers are only unique for a given resource type and may be used within"
+          + " the system for other resource types."
+)
 public class ResourceIdentity {
-  @NotBlank String identifier;
-  @NotBlank String system;
-  @NotBlank String resource;
+  @Schema(description = "Unique identifier for the resource type in the given system.")
+  @NotBlank
+  String identifier;
+
+  @Schema(description = "The system that defines the identifier.")
+  @NotBlank
+  String system;
+
+  @Schema(description = "A resource type narrows the identifier within the system.")
+  @NotBlank
+  @Pattern(regexp = "[-A-Za-z0-9_]")
+  String resource;
 }
